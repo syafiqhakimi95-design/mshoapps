59c456f6edb1cf32c95d7d6f73ec4fefd59afb20
syafiqhakimi95-design <syafiqhakimi95@gmail.com>
2026-01-04 14:11:26 +0800

    data.js

--- data.js
+++ data.js
@@ -3,50 +3,54 @@
 const profileData = {
     name: "Syafiq Hakimi",
     subtitle: "Optometry Lecturer | Content Creator",
-    logo: "logo.png" // Pastikan fail gambar ada
+    logo: "logo.png" // Pastikan fail gambar 'logo.png' ada dalam folder
 };
 
-// BAHAGIAN 1: BUTANG UTAMA (Tengah)
+// BAHAGIAN 1: SENARAI BUTANG (Tengah)
 const linkList = [
     // --- LINK PUBLIC (Tak perlu password) ---
     {
         title: "Amsler Grid with Report",
         url: "https://syafiqhakimi95-design.github.io/amsler-grid-app1/",
-        icon: "fa-solid fa-table-cells" // Ikon Grid
+        icon: "fa-solid fa-table-cells" 
     },
     {
         title: "Ametropia Simulator (+Presbyopia)",
         url: "https://syafiqhakimi95-design.github.io/Ametropia2/",
-        icon: "fa-solid fa-eye" // Ikon Mata
+        icon: "fa-solid fa-eye"
     },
     {
         title: "Astigmatism Simulator",
         url: "https://mshoapps.github.io/astigmatism-simulator/",
-        icon: "fa-solid fa-eye-low-vision" // Ikon Kabur
+        icon: "fa-solid fa-eye-low-vision"
     },
     {
         title: "Prism Simulator",
         url: "https://mshoapps.github.io/prism/",
-        icon: "fa-solid fa-play" // Bentuk segitiga (Prism)
+        icon: "fa-solid fa-play"
     },
     {
         title: "Manual Lensometer Simulator",
         url: "https://syafiqhakimi95-design.github.io/lensometer-apps/",
-        icon: "fa-solid fa-crosshairs" // Ikon Target Lensometer
+        icon: "fa-solid fa-crosshairs"
     },
 
     // --- LINK RAHSIA (Encrypted - Kena Password) ---
     {
         title: "Dichoptic Therapy Games",
-        // Kod rahsia yang Cikgu bagi:
         encryptedUrl: "U2FsdGVkX1/8Dm4n9Y9PmD0w78VHupSu6ZWj958XQlAptIxrwzEQgjI8iP3LFKwymx2YnvD2eekt0wHZVXYbG1VmT9jTg3OPNXpKvaf5D44=",
-        icon: "fa-solid fa-gamepad" // Ikon Game
+        icon: "fa-solid fa-gamepad"
     },
     {
         title: "Chess DT Games",
-        // Kod rahsia yang Cikgu bagi:
         encryptedUrl: "U2FsdGVkX1/WugJqKl49UoztT2vTNPQAFobOQrSjMI3hWgzEqqtnx/svg2lB4ZTk5NznwXI96RgrkxKSK7fxr+mIn/hmNVvNxnlOKFc8W2E=",
-        icon: "fa-solid fa-chess-knight" // Ikon Chess
+        icon: "fa-solid fa-chess-knight"
+    },
+    {
+        title: "JSU generator Table 4",
+        // Kod rahsia baru untuk Exam Pro:
+        encryptedUrl: "U2FsdGVkX18mLPRHGAdpX3JAl4/8M51YrZ8FcoSH3IfQa9yq/7M2bYR6/Wnuv7ySm1+eToTEAN2J1d2ia1secA==",
+        icon: "fa-solid fa-table" // Ikon Jadual
     }
 ];
 
